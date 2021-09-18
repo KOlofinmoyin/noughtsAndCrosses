aa3d5a93efe19aa54b434dae5218fd5e41028e5c
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-09-18 15:43:30 +0100

    feat: loop through top row and check for winner

--- noughtsAndCrosses.js
+++ noughtsAndCrosses.js
@@ -4,16 +4,15 @@ function board(currentPosition){
   if (currentPosition.innerText != "") return;
       currentPosition.innerText = currentPlayer;
       currentPlayer == 'X' ? currentPlayer = 'O' : currentPlayer = 'X';
-      checkBoardGame();
+      checkGameBoard();
 }
 
 function checkGameBoard(){
-  let firstOne = document.getElementById('0_0').innerText;
-  let firstTwo = document.getElementById('1_0').innerText;
-  let firstThree = document.getElementById('2_0').innerText;
-
-
-  if (firstOne == firstTwo && firstOne == firstThree) {alert("You've won");}
-
-
+    for (var i = 0; i < 2; i++) {
+      let firstOne = document.getElementById('0_'+i).innerText;
+      let firstTwo = document.getElementById('1_'+i).innerText;
+      let firstThree = document.getElementById('2_'+i).innerText;
+      if (firstOne == "") continue;
+      if (firstOne == firstTwo && firstOne == firstThree) {alert("You've won");}
+    }
 }
