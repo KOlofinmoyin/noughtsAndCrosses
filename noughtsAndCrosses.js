664b676ff916cd9ef55983920fb0737ed19ee966
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-09-18 15:32:44 +0100

    feat: add #checkGameBoard to check winner

--- noughtsAndCrosses.js
+++ noughtsAndCrosses.js
@@ -1,9 +1,19 @@
 let currentPlayer = 'X';
 
 function board(currentPosition){
-  if (currentPosition.innerText != "") {
-    return;
-  }
+  if (currentPosition.innerText != "") return;
       currentPosition.innerText = currentPlayer;
       currentPlayer == 'X' ? currentPlayer = 'O' : currentPlayer = 'X';
+      checkBoardGame();
+}
+
+function checkGameBoard(){
+  let firstOne = document.getElementById('0_0').innerText;
+  let firstTwo = document.getElementById('1_0').innerText;
+  let firstThree = document.getElementById('2_0').innerText;
+
+
+  if (firstOne == firstTwo && firstOne == firstThree) {alert("You've won");}
+
+
 }
