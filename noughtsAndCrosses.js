847f3ba793da7cf6a08a92fc9a2879ec6b74a729
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-09-20 16:48:42 +0100

    refactor: pass html ids directly to #checkForWinner

--- noughtsAndCrosses.js
+++ noughtsAndCrosses.js
@@ -8,22 +8,20 @@ function board(currentPosition){
 }
 
 function checkGameBoard(){
-    for (var i = 0; i < 2; i++) {
-      let rowOne = document.getElementById('0_'+i).innerText;
-      let rowTwo = document.getElementById('1_'+i).innerText;
-      let rowThree = document.getElementById('2_'+i).innerText;
+    for (var i = 0; i <= 2; i++) {
 
-      let columnOne = document.getElementById(i + '_0').innerText;
-      let columnTwo = document.getElementById(i + '_1').innerText;
-      let columnThree = document.getElementById(i + '_2').innerText;
-
-      checkForWinner(rowOne, rowTwo, rowThree);
-      checkForWinner(columnOne, columnTwo, columnThree);
+      checkForWinner(document.getElementById('0_'+i).innerText, document.getElementById('1_'+i).innerText, document.getElementById('2_'+i).innerText);
+      checkForWinner(document.getElementById(i + '_0').innerText, document.getElementById(i + '_1').innerText, document.getElementById(i + '_2').innerText);
     }
+
+    checkForWinner(document.getElementById('0_0').innerText, document.getElementById('1_1').innerText, document.getElementById('2_2').innerText);
+    checkForWinner(document.getElementById('0_2').innerText, document.getElementById('1_1').innerText, document.getElementById('2_0').innerText);
+
 }
 
 function checkForWinner(first, second, third){
   if (first != "" && first == second && first == third) {
     alert("You've won");
+    return;
   }
 }
